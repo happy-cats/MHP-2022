e797eee442bcabd1f32104d020194886c6a72c56
Markus Schreiber <Markus.Schreiber@cloudflight.io>
2022-03-05 21:33:16 +0100

    Add dqn support

--- src/main_dqn.py
+++ src/main_dqn.py
@@ -2,6 +2,7 @@ import attr
 from stable_baselines3.dqn.dqn import DQN
 import os
 import sys
+
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
@@ -11,7 +12,7 @@ from sumo_rl import SumoEnvironment
 
 
 @attr.s
-class QLearningParams:
+class DQNParams:
     route_file = attr.ib(default='data/osm-train-simple/osm.rou.xml')
     net_file = attr.ib(default='data/osm-train-simple/map.net.xml')
     out_file = attr.ib(default='outputs/osm-train-simple/dqn/')
@@ -19,14 +20,14 @@ class QLearningParams:
 
 
 if __name__ == '__main__':
-    params = QLearningParams()
+    params = DQNParams()
     env = SumoEnvironment(net_file=params.net_file,
-                                    route_file=params.route_file,
-                                    out_csv_name=params.out_file,
-                                    single_agent=True,
-                                    use_gui=True,
-                                    num_seconds=params.simulation_tile,
-                                    max_depart_delay=0)
+                          route_file=params.route_file,
+                          out_csv_name=params.out_file,
+                          single_agent=True,
+                          use_gui=True,
+                          num_seconds=params.simulation_tile,
+                          max_depart_delay=0)
     model = DQN(
         env=env,
         policy="MlpPolicy",
