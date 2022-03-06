5d590cd626d89dd059e124044398e4808fceb160
tinaavbelj <tina.avbelj@gmail.com>
2022-03-06 09:39:29 +0100

    Fixing error

--- frontend/src/components/SplashScreenComponent.vue
+++ frontend/src/components/SplashScreenComponent.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-container class="main">
     <v-img
       src="@/assets/svg/logo.svg"
@@ -7,10 +6,6 @@
       class="splash-screen"
       @click="hideSplash()"
     />
-=======
-  <v-container class="main" v-bind:style="{ backgroundImage: 'url(' + require('@/assets/svg/Breeze.svg') + ')' }">
-    <div></div>
->>>>>>> fcd7996dd073591fe3a4da9dc41b35f2a92d46e5
   </v-container>
 </template>
 
